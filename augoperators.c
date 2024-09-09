db30048462ad335650d611d393af84b9be1fea47
benniimaru <shaikabdulmalik185@gmail.com>
2024-09-09 20:42:01 +0530

    09/09/2024

---
+++ augoperators.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    //augumented assignment operators = used to replace a statement where an operator 
+    //takes a variaabke as one of its arguments 
+    //and then assigns the result back to the same variable 
+    // these are basically shortcuts forr the long programs    
+    int x = 10;
+    
+    //x = x + 1
+    //x+=1;
+    
+    // x = x + 2;
+    //x+=2;
+    
+    // x = x - 3;
+    //x-=3;
+    
+    // x = x * 4;
+    //x*=4;
+
+    //x = x / 5;
+    //x/=5;
+
+    //x = % 6;
+    //x%=6;
+
+    printf("%d",x);
+
+    return 0;
+
+}
