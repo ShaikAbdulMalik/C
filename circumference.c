b7dbb26ad076e3bbca74a43fdf0eb6822afb7da6
benniimaru <shaikabdulmalik185@gmail.com>
2024-09-11 20:45:18 +0530

    added area function

--- circumference.c
+++ circumference.c
@@ -4,11 +4,13 @@ int main(){
     const double PI = 3.1415;//'const' here is used to declare PI so that the user can't change value of PI. 
     double radius;
     double circumference;
-
+    double area;
     printf("\nEnter the radius of a circle:");
     scanf("%lf",&radius);
     circumference = 2 * PI * radius;
-    printf("circumference: %lf",circumference);
+    area = PI * radius * radius;
+    printf("circumference: %.2lf",circumference);
+    printf("\nArea: %.2lf",area);
     return 0;
 
 }
